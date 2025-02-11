2ea47fbd2ab820a7352d76585ace7d672353dfcf
AMF <af.dev@protonmail.com>
2025-02-11 14:11:13 +0900

    init

---
+++ 884.py
@@ -0,0 +1,36 @@
+# CONSTANTS
+CELL_A = "X"
+CELL_B = "O"
+CELL_EMPTY = "-"
+PLAYER_A = "Player A"
+PLAYER_B = "Palyer B"
+
+# FUNCTIONS
+def getIsPlayerAI(player):
+  print("getIsPlayerAI")
+
+def getBoard():
+  print("getBoard")
+
+def playAI():
+  print("playAI")
+
+def playPlayer():
+  print("playPlayer")
+
+# LOGIC
+AisAI = getIsPlayerAI("Player A")
+BisAI = getIsPlayerAI("Player B")
+board = getBoard()
+isGoing = True
+
+while isGoing:
+  if AisAI is True:
+    print("ok")
+  else:
+    print("ok")
+  if BisAI is True:
+    print("ok")
+  else:
+    print("ok")
+  isGoing = False
---
+++ README.txt
@@ -0,0 +1,5 @@
+884 is a m,n,k-game implementation that I've created to practice writing AIs for
+games.
+The general idea is that the players can play by placing their markers from top
+to bottom in columns. Either player will win if their stones connect in a line
+of four stones in any direction: vetical, horizontal, or diagonal.
